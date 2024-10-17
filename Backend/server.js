1fd7d331de467cf7f6901a2e3bf17aaf82e6de8b
Ali Mohammed <3li.ba.33@gmail.com>
2024-10-17 05:40:38 +0300

    Set up the server

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// // db connection
+// connectToDb((err) => {
+// 	if (!err) {
+// 		app.listen(port, () => {
+// 			console.log(`Listening on port ${port}`);
+// 		})
+// 	}
+// })
