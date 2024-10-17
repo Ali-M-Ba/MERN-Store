1ff2959fb4646147800cf87ac94f40bc121d1b19
Ali Mohammed <3li.ba.33@gmail.com>
2024-10-17 06:14:53 +0300

    Create the product model

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectToDb from "./config/db.js";
 import dotenv from "dotenv";
+import Product from "./models/product.model.js";
 
 const app = express();
 const port = 3000;
